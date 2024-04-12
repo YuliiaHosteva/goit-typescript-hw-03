f7a9c97ea945dad96f58ac880d3665698f3b7a2a
YuliiaHosteva <140606153+YuliiaHosteva@users.noreply.github.com>
2024-04-12 15:58:52 +0200

    refactor code.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,48 @@
+class Key {
+    private signature: number = Math.random();
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+        this.key = key;
+    }
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+    abstract openDoor(key: Key): void;
+    comeIn(tenants: Person): void {
+        if (this.door) {
+            this.tenants.push(tenants);
+        }
+    }
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new MyHouse();
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
